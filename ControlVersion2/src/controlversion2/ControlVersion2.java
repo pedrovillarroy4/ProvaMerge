2f7b8fe3617dc08f2a1353306da968ffc17ae5c9
ericg <ericg@LAPTOP-MI0RV32S>
2022-03-31 17:38:50 +0200

    poner un sout

---
+++ ControlVersion2/src/controlversion2/ControlVersion2.java
@@ -0,0 +1,16 @@
+package controlversion2;
+
+public class ControlVersion2 {
+ 
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Commit branchB");
+        
+        
+        
+        
+        
+    }
+    
+}
